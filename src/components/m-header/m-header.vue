3fe336e0f4216bf6ad872279015a053a31e38cf3
webEmmet <web_emmet@sina.com>
2017-09-08 17:57:15 +0800

    修改了头部组件的样式问题

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="m-header">
-    <div class="icon"></div>
     <h1 class="text">Free Style</h1>
     <router-link to="/user" class="mine" tag="div">
       <i class="icon-mine"></i>
